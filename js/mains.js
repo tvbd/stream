562a89906aab1d2906f7e999b03ec031d90e5c77
TVBD <45828705+tvbd@users.noreply.github.com>
2023-09-25 23:09:37 +0600

    Update mains.js

--- js/mains.js
+++ js/mains.js
@@ -12,7 +12,7 @@ function _0x415e(_0x2a5773, _0xbdbcf9) {
 }
 var _0x2d72eb = _0x415e;
 function _0x54cf() {
-  var _0x3b099b = ['player/jwnetflix.html?sv=', '#clickclapper', '#clickjw', '#clickkaltura', 'ready', 'click', 'player/plyr.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clicknetflix', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/kaltura.html?sv=', 'player/jwplayer.html?sv=', '2756mbGhPN'];
+  var _0x3b099b = ['player/jwnetflix.html?sv=', '#clickclapper', '#clickjw', '#clickvjs', '#clickjs', '#clickm3u', '#clicm3u2', '#clickoven', '#clickkaltura', 'ready', 'click', 'player/plyr.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clicknetflix', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/kaltura.html?sv=', 'player/jwplayer.html?sv=', 'player/oven.html?sv=', 'player/vjs.html?sv=', 'player/js.html?sv=', 'player/m3u.html?sv=', '//01234.fun/#/?url=', '2756mbGhPN'];
   _0x54cf = function () {
     return _0x3b099b;
   };
@@ -57,7 +57,7 @@ $(document).ready(function () {
     window.location.href = "player/vjs.html?sv=" + $('#link').val();
   });
 
-   $("#clickvjs").click(function () {
+   $("#clickjs").click(function () {
     window.location.href = "player/playerjs.html?sv=" + $('#link').val();
   });
 
@@ -65,7 +65,7 @@ $(document).ready(function () {
     window.location.href = "player/m3u.html?sv=" + $('#link').val();
   });
 
-   $("#clickm3u").click(function () {
+   $("#clickm3u2").click(function () {
     window.location.href = "http://01234.fun/#/?url=" + $('#link').val();
   });
 
