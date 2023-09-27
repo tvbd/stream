633758d87e39934d45eda34b61f00e486296f664
TVBD <45828705+tvbd@users.noreply.github.com>
2023-09-27 20:23:14 +0600

    Update and rename mains.js to main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,5 @@
+//Main JS Code
+
 $(document).ready(function () {
   $('h3').text('TVBD');
 });
@@ -12,7 +14,7 @@ function _0x415e(_0x2a5773, _0xbdbcf9) {
 }
 var _0x2d72eb = _0x415e;
 function _0x54cf() {
-  var _0x3b099b = ['player/jwnetflix.html?sv=', '#clickclapper', '#clickjw', '#clickvjs', '#clickjs', '#clickm3u', '#clicm3u2', '#clickoven', '#clickkaltura', 'ready', 'click', 'player/plyr.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clicknetflix', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/kaltura.html?sv=', 'player/jwplayer.html?sv=', 'player/oven.html?sv=', 'player/vjs.html?sv=', 'player/js.html?sv=', 'player/m3u.html?sv=', '//01234.fun/#/?url=', '2756mbGhPN'];
+  var _0x3b099b = ['player/js.html?sv=', '#clickclapper', '#clickvjs', '#clickkaltura', 'ready', 'click', 'player/oven.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clickjs', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/kaltura.html?sv=', 'player/vjs.html?sv=', '2756mbGhPN'];
   _0x54cf = function () {
     return _0x3b099b;
   };
@@ -35,7 +37,7 @@ function _0x54cf() {
 })(_0x54cf, 0x4968f);
 $(document).ready(function () {
   $("#clickjw").click(function () {
-    window.location.href = "player/jwplayer.html?sv=" + $("#link").val();
+    window.location.href = "player/vjs.html?sv=" + $("#link").val();
   });
   $('#clickply').click(function () {
     window.location.href = "player/kaltura.html?sv=" + $("#link").val();
@@ -44,34 +46,11 @@ $(document).ready(function () {
     window.location.href = "player/clapper.html?sv=" + $("#link").val();
   });
   $("#clickkaltura").click(function () {
-    window.location.href = "player/plyr.html?sv=" + $('#link').val();
-  });
-  $("#clicknetflix").click(function () {
-    window.location.href = "player/jwnetflix.html?sv=" + $("#link").val();
-  });
- $("#clickoven").click(function () {
     window.location.href = "player/oven.html?sv=" + $('#link').val();
   });
-
- $("#clickvjs").click(function () {
-    window.location.href = "player/vjs.html?sv=" + $('#link').val();
+  $("#clicknetflix").click(function () {
+    window.location.href = "player/js.html?sv=" + $("#link").val();
   });
-
-   $("#clickjs").click(function () {
-    window.location.href = "player/playerjs.html?sv=" + $('#link').val();
-  });
-
-     $("#clickm3u").click(function () {
-    window.location.href = "player/m3u.html?sv=" + $('#link').val();
-  });
-
-   $("#clickm3u2").click(function () {
-    window.location.href = "http://01234.fun/#/?url=" + $('#link').val();
-  });
-
-
-  
-
   $("#search").on('keyup', function () {
     var _0x4eb18d = $(this).val().toLowerCase();
     $(".col-sm-4").filter(function () {
@@ -79,15 +58,8 @@ $(document).ready(function () {
     });
   });
 });
-
-
-
-        $(document).ready(function(){
-
-            $("#menuimg").click(function(){
-
-               $("#drop").slideToggle("fast"); 
-
-            });
-
-        });
+$(document).ready(function () {
+  $("#menuimg").click(function () {
+    $("#drop").slideToggle("fast");
+  });
+});
