aa2ece5d622430d961fa271532853bab2e09ff50
TVBD <45828705+tvbd@users.noreply.github.com>
2023-09-28 00:36:15 +0600

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -14,7 +14,7 @@ function _0x415e(_0x2a5773, _0xbdbcf9) {
 }
 var _0x2d72eb = _0x415e;
 function _0x54cf() {
-  var _0x3b099b = ['player/playerjs.html?sv=', '#clickclapper', '#clickjw', '#clickkaltura', 'ready', 'click', 'player/oven.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clicknetflix', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/.html?sv=', 'player/vjs.html?sv=', '2756mbGhPN'];
+  var _0x3b099b = ['player/js.html?sv=', '#clickclapper', '#clickjw', '#clickkaltura', 'ready', 'click', 'player/oven.html?sv=', '3405XrSkFx', '321213yYBNCx', 'player/clapper.html?sv=', 'val', 'filter', 'href', '7915257ZqdtrX', '.col-sm-4', '3427216GLOOzU', 'toggle', '#clicknetflix', '#search', '#link', '3337271ldYQHU', 'text', 'toLowerCase', 'location', 'indexOf', '2524176ixXQfA', '186616phrAOC', '8FYnOsT', 'player/.html?sv=', 'player/vjs.html?sv=', '2756mbGhPN'];
   _0x54cf = function () {
     return _0x3b099b;
   };
@@ -49,7 +49,7 @@ $(document).ready(function () {
     window.location.href = "player/oven.html?sv=" + $('#link').val();
   });
   $("#clicknetflix").click(function () {
-    window.location.href = "player/playerjs.html?sv=" + $("#link").val();
+    window.location.href = "player/js.html?sv=" + $("#link").val();
   });
   $("#search").on('keyup', function () {
     var _0x4eb18d = $(this).val().toLowerCase();
